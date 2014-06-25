9fa34dd0f1a5273309ca0e651fced0979a2dbe4b
JeanMichel FRANCOIS <toutpt@gmail.com>
2014-06-25 18:25:10 +0200

    first release

---
+++ index.php
@@ -0,0 +1,253 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<title>Nantes m&eacute;tropole | Votre dossier immobilier d'entreprise</title>	
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0," />
+		<link rel="shortcut icon" type="image/x-icon" href="http://www.nantes-developpement.com/favicon.ico" />
+		 <link href="css/styles.css" rel="stylesheet" type="text/css" media="screen" />
+
+		<link rel="stylesheet" media="screen and (min-width: 1px) and (max-width: 480px)" href="css/mobile-320-480.css" type="text/css" />
+		<link rel="stylesheet" media="screen and (min-width: 481px) and (max-width: 990px)" href="css/mobile-480-768.css" type="text/css" /> 
+				 
+		<script type="text/javascript" src="js/jquery.min.js"></script>
+		<script type="text/javascript" src="js/jquery.localscroll.js"></script>
+		<script type="text/javascript" src="js/jquery.scrollTo.js"></script>
+		<script type="text/javascript" src="js/jquery.rsv.js"></script>
+		<script type="text/javascript" src="js/jquery.nyroModal.custom.js"></script>
+		<script type="text/javascript" src="js/main.js"></script>
+		
+	</head>	
+<body>
+	<div id="page">
+		<ul id="addthis_header" class="addthis">
+			<li class="addthis_toolbox addthis_default_style">
+				<a class="addthis_button_print"></a>
+				<a class="addthis_button_email"></a>
+			</li>
+			<li>Sauvegarder :&nbsp;</li>
+		</ul>
+		<hr class="clear" />
+		<div id="wrapper">
+			<div id="content">
+				<div id="main">
+					<div id="header">
+						<h1>Nom d'entreprise</h1>
+						<h2>votre dossier immobilier d’entreprise</h2>					
+					</div>
+					<div class="image_header"><img src="images/landing/votre-dossier-immobilier-d-entreprise.jpg" alt="votre dossier immobilier d’entreprise" /></div>
+					<div id="cahier_charges" class="motif">
+						<ul class="navigation">
+							<li class="un active"><a href="#cahier_charges" title="Votre cahier des charges"><span>Votre cahier des charges</span></a></li>
+							<li class="deux"><a href="#propositions" title="Propositions immobilières"><span>Propositions immobilières</span></a></li>
+							<li class="trois"><a href="#services" title="Les services de l'agence"><span>Les services de l'agence</span></a></li>
+							<li class="quatre"><a href="#territoire" title="Découverte du territoire"><span>Découverte du territoire</span></a></li>
+							<li class="mobile telecharger_pdf"><a onclick="window.open(this.href); return false;" href="###" title="Télécharger votre dossier au format PDF"><img alt="Télécharger votre dossier au format PDF" src="images/landing/ico-pdf-mobile.gif"></a></li>
+							<li class="mobile demande_contact"><a href="mailto:cduveau@nantes-developpement.com?subject=RE%20:%20Demande%20d'informations " title="Demande de contact"><img alt="Demande de contact" src="images/landing/ico-contact-mobile.gif"></a></li>
+						</ul>
+						<div class="inner">						
+							<ul class="colleft">
+								<li><strong>Type de local : </strong><span>Lorem ipsum dolor sit amet donec augue.</span></li>
+								<li><strong>Type d’occupation : </strong><span>Pharetra ac libero tempor, adipiscing dictum orci. Donec ultricies.</span></li>
+								<li><strong>Echéance : </strong><span>Pharetra ac libero tempor, adipiscing dictum orci. Donec ultricies.</span></li>
+								
+							</ul>
+							<ul class="colright">
+								<li><strong>Superficie : </strong><span>Consectetur adipiscing elit.</span></li>
+								<li><strong>Echéance : </strong><span>cursus. Praesent rhoncus.</span></li>
+								<li><strong>Effectif à terme : </strong><span>Estibulum nisl.</span></li>
+								
+							</ul>
+							<hr class="clear" />
+						</div>					
+					</div>
+					<div id="propositions" class="motif">
+						<ul class="navigation">
+							<li class="un"><a href="#cahier_charges" title="Votre cahier des charges"><span>Votre cahier des charges</span></a></li>
+							<li class="deux active"><a href="#propositions" title="Propositions immobilières"><span>Propositions immobilières</span></a></li>
+							<li class="trois"><a href="#services" title="Les services de l'agence"><span>Les services de l'agence</span></a></li>
+							<li class="quatre"><a href="#territoire" title="Découverte du territoire"><span>Découverte du territoire</span></a></li>
+							<li class="mobile telecharger_pdf"><a onclick="window.open(this.href); return false;" href="###" title="Télécharger votre dossier au format PDF"><img alt="Télécharger votre dossier au format PDF" src="images/landing/ico-pdf-mobile.gif"></a></li>
+							<li class="mobile demande_contact"><a href="mailto:cduveau@nantes-developpement.com?subject=RE%20:%20Demande%20d'informations " title="Demande de contact"><img alt="Demande de contact" src="images/landing/ico-contact-mobile.gif"></a></li>						
+						</ul>
+						<div class="inner">
+							<h2><strong>7 offres de biens</strong> sélectionnées pour vous</h2>
+							<div id="map">
+							<!-- IFRAME MAP -->
+							</div>
+							<div class="liste_offre">
+								<ol>
+									<li><strong>Lorem ipsum dolor sit amet sectetur.</strong> Praesent eget porta arcused faucibus.</li>
+									<li><strong>Lorem ipsum dolor sit amet sectetur.</strong> Praesent eget porta arcused faucibus.</li>
+									<li><strong>Lorem ipsum dolor sit amet sectetur.</strong> Praesent eget porta arcused faucibus.</li>
+									<li><strong>Lorem ipsum dolor sit amet sectetur.</strong> Praesent eget porta arcused faucibus.</li>
+									<li><strong>Lorem ipsum dolor sit amet sectetur.</strong> Praesent eget porta arcused faucibus.</li>
+									<li><strong>Lorem ipsum dolor sit amet sectetur.</strong> Praesent eget porta arcused faucibus.</li>
+									<li><strong>Lorem ipsum dolor sit amet sectetur.</strong> Praesent eget porta arcused faucibus.</li>
+								</ol>
+							</div>
+						</div>
+					</div>
+					<div id="services" class="motif">
+						<ul class="navigation">
+							<li class="un"><a href="#cahier_charges" title="Votre cahier des charges"><span>Votre cahier des charges</span></a></li>
+							<li class="deux"><a href="#propositions" title="Propositions immobilières"><span>Propositions immobilières</span></a></li>
+							<li class="trois active"><a href="#services" title="Les services de l'agence"><span>Les services de l'agence</span></a></li>
+							<li class="quatre"><a href="#territoire" title="Découverte du territoire"><span>Découverte du territoire</span></a></li>
+							<li class="mobile telecharger_pdf"><a onclick="window.open(this.href); return false;" href="###" title="Télécharger votre dossier au format PDF"><img alt="Télécharger votre dossier au format PDF" src="images/landing/ico-pdf-mobile.gif"></a></li>
+							<li class="mobile demande_contact"><a href="mailto:cduveau@nantes-developpement.com?subject=RE%20:%20Demande%20d'informations " title="Demande de contact"><img alt="Demande de contact" src="images/landing/ico-contact-mobile.gif"></a></li>
+						</ul>
+						<div class="inner">
+							<div id="qui_sommes_nous" class="active onglet">
+								<h2>A Nantes, placez vos projets d’entreprise <strong>sous une bonne étoile</strong></h2>
+								<div class="colleft">
+									<strong>Créée en 1998 par la communauté urbaine de Nantes, l’agence de développement économique Nantes Métropole Développement accompagne les entreprises dans leur implantation sur le territoire.</strong><br />
+									<br />
+									<strong>Elle a pour principales missions :</strong>
+									<ul>
+										<li>L’accompagnement des entreprises</li>
+										<li>La promotion du territoire</li>
+										<li>L’animation des acteurs économiques</li>
+									</ul>
+								</div>
+								<div class="colright">
+									<p>L’agence compte 18 collaborateurs. Elle est présidée par Charles Gautier, Vice-Président au développement économique de Nantes Métropole et dirigée par Patrick Robert.</p>
+									<p>Depuis 1998, Nantes Métropole Développement a accompagné plus de 800 entreprises dans leur installation sur l’agglomération, contribuant à la création de plus de 12 000 emplois.</p>
+									<a href="htttp://www.nantes-developpement.com" title="">www.nantes-developpement.com</a>
+								</div>
+								<hr class="clear" />
+							</div>
+							<div id="service" class="inactive onglet">
+								<h2>A Nantes, placez vos projets d’entreprise <strong>sous une bonne étoile</strong></h2>
+								<div class="colleft">
+									<strong>Nous partageons vos objectifs</strong><br>
+									Nous apportons les réponses adaptées à vos besoins en fonction de votre cahier des charges et ce, sans contrepartie financière.<br>
+									<br>
+									<strong>Nous identifions l’offre territoriale</strong><br>
+									Offre immobilière et foncière, potentiel marché, recrutement, nous apportons tous les informations nécessaires à votre prise de décision.<br>
+									<br>
+									<strong>Nous mobilisons nos partenaires</strong><br>
+									Nous collaborons avec les experts locaux pour faciliter votre implantation : commercialisateurs immobiliers, cabinets RH, établissements financiers, administrations... 
+								</div>
+								<div class="colright">
+									<strong>Nous accompagnons vos salariés</strong><br>
+									Présentation de l’agglomération, recherche d’un logement, emploi du conjoint... Nous facilitons l’intégration de vos salariés sur la métropole nantaise.<br>
+									<br>
+									<strong>Nous accélérons votre intégration</strong><br>
+									Communication sur votre implantation, mise en relation avec les prescripteurs, participation aux événements locaux, nous vous aidons dans votre intégration.
+								</div>
+								<hr class="clear">
+							</div>
+						</div>
+						<ul class="nav_footer">						
+							<li><a href="#service">Les services</a></li>
+							<li class="active"><a href="#qui_sommes_nous" >Qui sommes-nous ?</a></li>
+						</ul>
+						<hr class="clear" />
+					</div>
+					<div id="territoire" class="motif">
+						<ul class="navigation">
+							<li class="un"><a href="#cahier_charges" title="Votre cahier des charges"><span>Votre cahier des charges</span></a></li>
+							<li class="deux"><a href="#propositions" title="Propositions immobilières"><span>Propositions immobilières</span></a></li>
+							<li class="trois"><a href="#services" title="Les services de l'agence"><span>Les services de l'agence</span></a></li>
+							<li class="quatre active"><a href="#territoire" title="Découverte du territoire"><span>Découverte du territoire</span></a></li>
+							<li class="mobile telecharger_pdf"><a onclick="window.open(this.href); return false;" href="###" title="Télécharger votre dossier au format PDF"><img alt="Télécharger votre dossier au format PDF" src="images/landing/ico-pdf-mobile.gif"></a></li>
+							<li class="mobile demande_contact"><a href="mailto:cduveau@nantes-developpement.com?subject=RE%20:%20Demande%20d'informations " title="Demande de contact"><img alt="Demande de contact" src="images/landing/ico-contact-mobile.gif"></a></li>						
+						</ul>
+						<div class="inner">
+							<div class="contenu_article active onglet" id="attractivite">
+								<div class="colleft">
+									<h2>Une ville <strong>attractive</strong></h2>
+									<strong>Nantes se place régulièrement en tête des<br> palmarès des villes où il fait bon vivre</strong>
+									<ul>
+										<li>Une métropole de 600 000 habitants</li>
+										<li>5<sup>ème</sup> agglomération française par sa<br>croissance démographique</li>
+										<li>+ 60 000 emplois en 10 ans</li>
+										<li>Capitale verte de l’Europe en 2013</li>
+									</ul>
+								</div>
+								<div class="colright">
+									<a href="http://youtu.be/-wuND5x6IU8" class="nyroModal" rel="gal">
+									<img alt="Nantes en projets" src="images/landing/nantes-en-projet.gif"></a>
+									<span class="idgrlg">Nantes <span class="idblbo">en 2 minutes</span></span>
+								</div>
+								<hr class="clear">
+							</div>
+							<div class="contenu_article inactive onglet" id="telechargement">
+								<div class="colleft">								
+								</div>
+								<div class="colright">								
+								</div>
+								<hr class="clear">
+							</div>
+						</div>
+						<ul class="nav_footer">						
+							<li class="active"><a href="#attractivite">Nantes</a></li>
+							<li><a href="###" >Téléchargement</a></li>
+						</ul>
+						<hr class="clear" />
+					</div>					
+				</div>
+			</div>
+			<div id="leftbar">
+				<div class="inner">
+					<h4><a title="" href="###" class="telecharger_pdf" onclick="window.open(this.href); return false;"><em>Télécharger</em> le détails de vos offres</a></h4>
+					<a title="" href="###" class="telecharger_pdf_image" onclick="window.open(this.href); return false;">
+						<img src="images/landing/telechargement-pdf.png" alt="Télécharger votre dossier au format PDF" />
+					</a>
+					<hr class="clear" />
+				</div>
+			</div>
+			<div id="rightbar">
+				<div class="mobile bg_layer"></div>
+				<div id="bloc_contact">
+					<img src="images/landing/claire-duveau.jpg" alt="Claire DUVEAU" />
+					<h4>Votre contact</h4>
+					<address>Claire <strong>DUVEAU</strong><br>
+						<em>Chargée de mission</em>
+						<span><strong>Tél.</strong> : 02 40 35 28 88</span>
+						<br>
+					</address>
+					<a href="mailto:cduveau@nantes-developpement.com?subject=RE%20:%20Demande%20d'informations ">
+						<img alt="Me contacter" src="images/landing/me-contacter.gif">
+					</a>
+					<ul>
+						<li><a class="twitter" title="Twitter" href="https://twitter.com/nantesdev"></a></li>
+						<li><a class="viadeo" title="Viadeo" href="http://www.viadeo.com/fr/profile/claire.duveau"></a></li>
+					</ul>
+					<hr class="clear">
+					<span class="mobile">Masquer</span>
+				</div>
+			</div>
+			<hr class="clear" />
+			<ul id="addthis_footer" class="addthis">
+				<li class="addthis_toolbox addthis_default_style">
+					<a class="addthis_button_print"></a>
+					<a class="addthis_button_email"></a>
+				</li>
+				<li>Sauvegarder :&nbsp;</li>
+			</ul>
+			<hr class="clear" />
+		</div>
+		<div id="footer">
+			<a href="http://www.nantes-developpement.com" title="www.nantes-developpement.com" onclick="window.open(this.href); return false;">Nantes Métropole Développement</a> est un outil opérationnel au service des entreprises, créé et financé par la <a href="http://www.nantesmetropole.fr" title="www.nantesmetropole.fr" onclick="window.open(this.href); return false;">communauté urbaine	de Nantes</a>.<br /> Visiter notre site <a href="http://www.nantes-developpement.com" title="www.nantes-developpement.com" onclick="window.open(this.href); return false;">www.nantes-developpement.com</a> | <a href="http://www.nantes-developpement.com/formulaires/contactez-nous" title="Contacter Nantes M&eacute;tropole D&eacute;veloppement" onclick="window.open(this.href); return false;">Nous contacter</a>
+		</div>			
+	</div>
+
+	<script type="text/javascript" src="http://s7.addthis.com/js/250/addthis_widget.js"></script>
+	<script type="text/javascript">
+
+	  var _gaq = _gaq || [];
+	  _gaq.push(['_setAccount', 'UA–']);
+	  _gaq.push(['_trackPageview']);
+
+	  (function() {
+		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	  })();
+
+	</script>
+</body>
+
+</html>
