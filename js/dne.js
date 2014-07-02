c965404c784b6c697486283811b920b822e51b08
JeanMichel FRANCOIS <toutpt@gmail.com>
2014-07-02 14:39:14 +0200

    add social networks

--- js/dne.js
+++ js/dne.js
@@ -27,7 +27,32 @@ angular.module('dne.controllers').controller('DNEController',
             lng:-1.5603,
             zoom: 11
         };
-
+        $scope.socialnetworks = {};
+        $scope.socialnetworks['43e34dab-0757-ca3d-f439-4c52ebbadc33'] = [
+            {id: 'twitter', title:'Twitter', url:'https://twitter.com/ouzilleau_nmd'},
+            {id: 'linkedin', title:'LinkedIn', url:'http://fr.linkedin.com/pub/b%C3%A9r%C3%A9nice-ouzilleau/11/96b/1a5'},
+            {id: 'viadeo', title:'Viadeo', url:'http://fr.viadeo.com/fr/profile/berenice.ouzilleau'}
+        ];
+        $scope.socialnetworks['f23c9c46-461d-b014-f87e-4c52ebee0f86'] = [
+            {id: 'twitter', title:'Twitter', url:'https://twitter.com/SandrineGauvry'},
+            {id: 'linkedin', title:'LinkedIn', url:'http://fr.linkedin.com/pub/sandrine-gauvry/14/285/753'},
+            {id: 'viadeo', title:'Viadeo', url:'http://fr.viadeo.com/fr/profile/sandrine.gauvry'}
+        ];
+        $scope.socialnetworks['dab2c008-3956-2c85-460a-4d3d76df321c'] = [
+            {id: 'twitter', title:'Twitter', url:'https://twitter.com/ClaireDuveau'},
+            {id: 'linkedin', title:'LinkedIn', url:'http://fr.linkedin.com/in/claireduveau'},
+            {id: 'viadeo', title:'Viadeo', url:'http://fr.viadeo.com/fr/profile/claire.duveau'}
+        ];
+        $scope.socialnetworks['2cb4f040-0bcf-80c4-c072-4c52eb727460'] = [
+            {id: 'viadeo', title:'Viadeo', url:'http://fr.viadeo.com/fr/profile/julie.letan'}
+        ];
+/*        $scope.socialnetworks[''] = [
+            {id: 'linkedin', title:'LinkedIn', url:'https://www.linkedin.com/pub/julie-cheruette/53/798/146'},
+            {id: 'viadeo', title:'Viadeo', url:'http://fr.viadeo.com/fr/profile/julie.cheruette'}
+        ];*/
+        $scope.socialnetworks['c3da9211-7b7a-aa96-084d-4c52eb7b7336'] = [
+            {id: 'viadeo', title:'Viadeo', url:'http://fr.viadeo.com/fr/profile/frederic.chauchet'}
+        ];
         $scope.dossier = {
             name: 'Project title',
             date: 'JJ/MM/AA',
@@ -41,6 +66,13 @@ angular.module('dne.controllers').controller('DNEController',
             },
             account:{
                 name: 'Nom d\' entreprise'
+            },
+            assigned_user: {
+                id: 'f23c9c46-461d-b014-f87e-4c52ebee0f86',
+                last_name: 'Gauvery',
+                first_name: 'Sandrine',
+                title: 'Chargé(e)s de mission',
+                phone_work: '02 40 35 63 57'
             }
         };
         $scope.offres = [
@@ -85,7 +117,6 @@ angular.module('dne.controllers').controller('DNEController',
             };
             var offresDiffuses = JSON.parse(maps.drawnFeatures) || [];
             var offresPerennes = maps.projects || [];
-            debugger;
             if (offresPerennes.length > 0){
                 for (var i = 0; i < offresPerennes.length; i++) {
                     $scope.offres.push({
