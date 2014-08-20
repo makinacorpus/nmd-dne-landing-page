f4a48b539b5bfe1be558d02e5c4e2d927369f551
JeanMichel FRANCOIS <toutpt@gmail.com>
2014-08-20 17:02:41 +0200

    fix(landingpage): embed url

--- js/dne.js
+++ js/dne.js
@@ -92,7 +92,7 @@ angular.module('dne.controllers').controller('DNEController',
         $scope.today = new Date();
         $scope.dossierID = $location.url().split('/')[1];
 
-        var mapURL = baseURL + '/embed/#/';
+        var mapURL = baseURL + '/embed/#';
         var pdfURL = baseURL + '/downloads/';
         $scope.mapURL = $sce.trustAsResourceUrl(mapURL + $scope.dossierID);
         $http.get('/view/dne/'+ $scope.dossierID).then(function(data){
